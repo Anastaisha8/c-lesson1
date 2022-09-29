79b49b13add739503cdee5629de0c315a8c08de6
Nasty <balandina2balandina@yandex.ru>
2022-09-29 09:34:14 +0300

    added exercie 8

---
+++ 8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+bool not = true;
+while (i<=N)
+{
+    if (i%2 == 0)
+    {
+        Console.Write(i + ",");
+        not = false;
+
+    }
+    i++;
+}
+if (not)
+{
+    Console.WriteLine("Четных чисел нет");
+}
