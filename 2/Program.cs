3f928ce8cb5e5ef7c36734cf664235ed3ca8cc11
Nasty <balandina2balandina@yandex.ru>
2022-09-29 09:01:15 +0300

    add exercise 2

---
+++ 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+Console.Clear();
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1 > number2)
+{
+    int max= number1;
+    int min= number2;
+    Console.WriteLine($"Максимальное число = {max}");
+    Console.WriteLine($"Минимальное число = {min}");
+   
+}
+if (number1<number2)
+{ 
+    int max = number2;
+    int min = number1;
+    Console.WriteLine($"Максимальное число = {max}");
+    Console.WriteLine($"Минимальное число = {min}");
+
+}
+if (number1 == number2)
+{
+    Console.WriteLine("Введенные числа равны.");
+}
